5ba1f89cc9042540e535fc87a765108f4f9c8a45
merndeveloperdhke <merndevelpler@gmail.comm>
2024-04-09 17:22:55 +0600

    nav and banner

--- common.js
+++ common.js
@@ -1,56 +1,35 @@
-function setElement(elementId, value){
-    const table_body = document.getElementById(elementId);
-    const ticketPrice = perTicketPrice('per_ticket_price')
-    const tableRow = document.createElement('tr');
-    const td = document.createElement('td');
-    td.innerText = value;
-    const td1 = document.createElement('td');
-    td1.innerText = 'Economic';
-    const td2 = document.createElement('td');
-    td2.innerText = ticketPrice;
-    tableRow.appendChild(td)
-    tableRow.appendChild(td1)
-    tableRow.appendChild(td2)
-    table_body.appendChild(tableRow);
-    //seat count
-    const seat_count_string = document.getElementById('seat_count');
-    const seat_count = parseInt(seat_count_string.innerText);
-    seatCount('seat_count')
-// seat left
-seatLeft('seat_left', seat_count);
-totalPrice('total_price', ticketPrice);
-    return table_body;
-};
+
 
 //Per ticket price
-function perTicketPrice(elementId){
+function perTicketPrice(elementId) {
     const per_ticket_price_string = document.getElementById(elementId);
     const per_ticket_price = parseInt(per_ticket_price_string.innerText);
     return per_ticket_price;
 };
 
 //Total price
-function totalPrice(elementId, value){
+function totalPrice(elementId, value) {
     const total_price_string = document.getElementById(elementId);
     const totalPriceValue = parseInt(total_price_string.innerText);
-    total_price_string.innerText = totalPriceValue + value ;
+    total_price_string.innerText = totalPriceValue + value;
     return total_price_string;
-   
+
 };
 //Grand Total price
-function grandTotal(){
-    const grandTotalString = document.getElementById('grand_total');
+function grandTotal(elementId) {
+    const grandTotalString = document.getElementById(elementId);
     const total_price_string = document.getElementById('total_price');
     const totalPriceValue = parseInt(total_price_string.innerText);
-    grandTotalString.innerText = totalPriceValue ;
+    grandTotalString.innerText = totalPriceValue;
+    console.log(grandTotalString);
     return grandTotalString;
 }
 
-    
-   
+
+
 
 //seat count
-function seatCount(elementId){
+function seatCount(elementId) {
     const seat_count_string = document.getElementById(elementId);
     const seat_count = parseInt(seat_count_string.innerText);
     seat_count_string.innerText = seat_count + 1;
@@ -59,58 +38,64 @@ function seatCount(elementId){
 
 
 // Seat left
-function seatLeft(elementId, value){
+function seatLeft(elementId) {
     const seat_left_String = document.getElementById(elementId);
-const seat_Left = parseInt(seat_left_String.innerText);
-seat_left_String.innerText = seat_Left - value;
-return seat_left_String;
+    const seat_Left = parseInt(seat_left_String.innerText);
+    seat_left_String.innerText = seat_Left - 1;
+    return seat_left_String;
 };
 
 //apply coupon button
-function applyCoupon(){
-// apply coupon field
-const apply_coupon_field_string = document.getElementById('apply_coupon_field');
-const apply_coupon_value = apply_coupon_field_string.value;
-console.log(apply_coupon_field_string,apply_coupon_value);
-if(apply_coupon_value === 'NEW15'){
-    const total_price_string = document.getElementById('total_price');
-    const totalPriceValue = parseInt(total_price_string.innerText);
-    const discount = totalPriceValue * 0.10;
-    const discountPrice = document.getElementById('total_discount');
-    discountPrice.innerText = discount;
-    const applyParent = document.getElementById('applyParent')
-    applyParent.setAttribute('hidden', true)
-    
-     //Grand Total price
-const grand_total_price_string = document.getElementById('grand_total');
-const grand_total_price = parseInt(grand_total_price_string.innerText);
-grand_total_price_string.innerText = totalPriceValue - discountPrice.innerText
-    console.log(discount);
-    console.log(grand_total_price_string);
-   
-}else if(apply_coupon_value === 'Couple20'){
-    const total_price_string = document.getElementById('total_price');
-    const totalPriceValue = parseInt(total_price_string.innerText);
-    const discount = totalPriceValue * 0.20;
-    const discountPrice = document.getElementById('total_discount');
-    discountPrice.innerText = discount;
-    //Grand Total price
-const grand_total_price_string = document.getElementById('grand_total');
+function applyCoupon() {
+    // apply coupon field
+    const apply_coupon_field_string =getElement('apply_coupon_field');
+    const apply_coupon_value = apply_coupon_field_string.value;
+    console.log(apply_coupon_field_string, apply_coupon_value);
+    if (apply_coupon_value === 'NEW15') {
+        const total_price_string = getElement('total_price');
+        const totalPriceValue = parseInt(total_price_string.innerText);
+        const discount = totalPriceValue * 0.10;
+        const discountPrice =getElement('total_discount');
+        discountPrice.innerText = discount;
+        const applyParent = getElement('applyParent');
+        applyParent.setAttribute('hidden', true)
 
-grand_total_price_string.innerText = totalPriceValue - discountPrice.innerText;
-const applyParent = document.getElementById('applyParent')
-    applyParent.setAttribute('hidden', true)
-    console.log(discount);
-    console.log(grand_total_price_string.innerText);
-}
-else{
-    alert('wrong coupon code')
-    console.log('wrong');
- 
-    
-}
+        //Grand Total price
+        const grand_total_price_string = getElement('grand_total');
+        grand_total_price_string.innerText = totalPriceValue - discountPrice.innerText
+        console.log(discount);
+        console.log(grand_total_price_string);
+
+    } else if (apply_coupon_value === 'Couple20') {
+        const total_price_string =getElement('total_price');
+        const totalPriceValue = parseInt(total_price_string.innerText);
+        const discount = totalPriceValue * 0.20;
+        const discountPrice =getElement('total_discount');
+        discountPrice.innerText = discount;
+        //Grand Total price
+        const grand_total_price_string =getElement('grand_total');
+
+        grand_total_price_string.innerText = totalPriceValue - discountPrice.innerText;
+        const applyParent = getElement('applyParent');
+        applyParent.setAttribute('hidden', true);
+    }
+    else {
+        alert('wrong coupon code')
+        console.log('wrong');
+        const ticketPrice = perTicketPrice('per_ticket_price')
+        totalPrice('total_price', ticketPrice);
+        const applyParent = getElement('applyParent')
+        applyParent.removeAttribute('hidden', true)
+
+
+    }
 };
 
+//get element
+function getElement(elementId){
+    const element = document.getElementById(elementId);
+    return element;
+}
 
 
 
--- script.js
+++ script.js
@@ -33,9 +33,33 @@ for(const btn of allBtn){
   btn.addEventListener('click', function(e){
     const selectedBtn = e.target.innerText;
     setElement('tableBody', selectedBtn);
-    btn.classList.add('bg-green-500', true)
+    // btn.setAttribute('disabled', true);
+    btn.classList.add('bg-green-500', true);
   })
   
+};
+
+function setElement(elementId, value) {
+  const table_body = document.getElementById(elementId);
+  const ticketPrice = perTicketPrice('per_ticket_price')
+  const tableRow = document.createElement('tr');
+  const td = document.createElement('td');
+  td.innerText = value;
+  const td1 = document.createElement('td');
+  td1.innerText = 'Economic';
+  const td2 = document.createElement('td');
+  td2.innerText = ticketPrice;
+  tableRow.appendChild(td)
+  tableRow.appendChild(td1)
+  tableRow.appendChild(td2)
+  table_body.appendChild(tableRow);
+  //seat count
+  seatCount('seat_count')
+  // seat left
+  seatLeft('seat_left');
+  totalPrice('total_price', ticketPrice);
+  grandTotal('grand_total')
+  return table_body;
 };
 
 // next button
