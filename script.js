7170c517bf7c69f8af9d3807d9ab2d2e7e230f5b
merndeveloperdhke <merndevelpler@gmail.comm>
2024-04-12 21:19:33 +0600

    header and banner done

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
-/* function getSeatDetails(){
-    const btnA = document.getElementById('a1')
-    const btnAText = btnA.innerText;
-    //Per ticket price
-const per_ticket_price_string = document.getElementById('per_ticket_price');
-const per_ticket_price = parseInt(per_ticket_price_string.innerText);
-    const price_string = document.getElementById('price');
-const price = parseInt(price_string) ;
-const table_body = document.getElementById('tableBody')
-    const tableRow = document.createElement('tr');
-    const td = document.createElement('td');
-    td.innerText = btnAText;
-    const td1 = document.createElement('td');
-    td1.innerText = 'Economy';
-    const td2 = document.createElement('td');
-    td2.innerText = per_ticket_price * 1;
-    table_body.appendChild(tableRow)
-    tableRow.appendChild(td)
-    tableRow.appendChild(td1)
-    tableRow.appendChild(td2);
-    const total_price_string = document.getElementById('total_price');
-    const totalPriceValue = parseInt(total_price_string.innerText);
-total_price_string.innerText = totalPriceValue + per_ticket_price 
-console.log(total_price);
-let count = 0;
-const seatCount = document.getElementById('seat_count');
-seatCount.innerText = count + 1
 
-} */
 
 const allBtn = document.getElementsByClassName('seat_no');
-for(const btn of allBtn){
-  btn.addEventListener('click', function(e){
+for (const btn of allBtn) {
+  btn.addEventListener('click', function (e) {
     const selectedBtn = e.target.innerText;
     setElement('tableBody', selectedBtn);
-    // btn.setAttribute('disabled', true);
-    btn.classList.add('bg-green-500', true);
+    btn.disabled = true;
+    btn.style.backgroundColor = "green";
+    btn.style.color = 'white'
   })
-  
+
 };
 
 function setElement(elementId, value) {
   const table_body = document.getElementById(elementId);
   const ticketPrice = perTicketPrice('per_ticket_price')
   const tableRow = document.createElement('tr');
+
   const td = document.createElement('td');
   td.innerText = value;
   const td1 = document.createElement('td');
@@ -51,8 +25,9 @@ function setElement(elementId, value) {
   td2.innerText = ticketPrice;
   tableRow.appendChild(td)
   tableRow.appendChild(td1)
-  tableRow.appendChild(td2)
+  tableRow.appendChild(td2);
   table_body.appendChild(tableRow);
+  tableRow.classList.add('bg-gray-600', 'text-white')
   //seat count
   seatCount('seat_count')
   // seat left
@@ -63,9 +38,9 @@ function setElement(elementId, value) {
 };
 
 // next button
-function nextBtnClick(){
+function nextBtnClick() {
   const nameField = document.getElementById('name');
-  const name = nameField.value ;
+  const name = nameField.value;
   console.log(typeof name);
   const numberField = document.getElementById('number');
   const number = numberField.value;
@@ -73,7 +48,8 @@ function nextBtnClick(){
   const email = emailField.value;
   console.log(name, number, email);
 
- }
+
+}
 
 
 
